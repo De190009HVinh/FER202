6ea76d5d0ce8733e80ffbd6dda9de6fe1321f099
VinhHoang03 <vinh03072005@gmail.com>
2026-01-07 14:19:43 +0700

    add new image

---
+++ Slot1/ex1/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div className="App">
+      <h1>Welcome to Top 1 The Gioi</h1>
+      <h2>Suck my dick </h2>
+        <img src="\image\v1.png" alt="anhsec" width="800" height="500"/>
+    </div> 
+  );
+}
+export default Footer;
